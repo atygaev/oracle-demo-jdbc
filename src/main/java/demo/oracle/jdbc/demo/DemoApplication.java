dc300ce89c6ef132dd6dad7c5e2290caf3fd5fac
Maks Atygaev <atygaev.mi@gmail.com>
2021-09-01 11:51:40 +0700

    Add table

--- src/main/java/demo/oracle/jdbc/demo/DemoApplication.java
+++ src/main/java/demo/oracle/jdbc/demo/DemoApplication.java
@@ -2,6 +2,13 @@ package demo.oracle.jdbc.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,3 +16,34 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 }
+
+@Entity
+@Table(name = "persons")
+class Person {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "fullName")
+    private String fullName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
+
+interface PersonDao extends JpaRepository<Person, Integer> {}
