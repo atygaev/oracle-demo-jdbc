f63d0a1bdc006df66c78d765e11fa5effadaee3e
Maks Atygaev <atygaev.mi@gmail.com>
2021-09-01 11:44:51 +0700

    Initial

---
+++ src/test/java/demo/oracle/jdbc/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.oracle.jdbc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
